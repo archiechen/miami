22a004dfd872c2a64cf0b12c75c0d44bc9497700
archiechen <chenyachuan@hotmail.com>
2012-11-25 18:41:17 +0800

    Review显示有效工时比例

--- tests/review_models_test.py
+++ tests/review_models_test.py
@@ -20,37 +20,37 @@ class ReviewModelsTest(unittest.TestCase):
         db.session.add(team)
         db.session.commit()
 
-        task = Task('title1', 'detail', status='DONE', price=2, estimate=4, team=Team.query.get(1),start_time=miami.views.get_last_monday().replace(hour=10))
+        task = Task('title1', 'detail', status='DONE', price=2, estimate=4, team=Team.query.get(1), start_time=miami.views.get_last_monday().replace(hour=10))
         ts = TimeSlot(miami.views.get_last_monday().replace(hour=10), 7200, User.query.get(1))
         task.time_slots.append(ts)
         db.session.add(task)
         db.session.commit()
 
-        task = Task('title2', 'detail2', status='DONE', price=2, estimate=4, team=Team.query.get(1),start_time=miami.views.get_last_monday().replace(hour=12))
+        task = Task('title2', 'detail2', status='DONE', price=2, estimate=4, team=Team.query.get(1), start_time=miami.views.get_last_monday().replace(hour=12))
         ts = TimeSlot(miami.views.get_last_monday().replace(hour=12), 3600, User.query.get(1), partner=User.query.get(2))
         task.time_slots.append(ts)
         db.session.add(task)
         db.session.commit()
 
-        task = Task('title3', 'detail3', status='DONE', price=2, estimate=4, team=Team.query.get(1),start_time=miami.views.get_last_monday().replace(hour=14))
+        task = Task('title3', 'detail3', status='DONE', price=2, estimate=4, team=Team.query.get(1), start_time=miami.views.get_last_monday().replace(hour=14))
         ts = TimeSlot(miami.views.get_last_monday().replace(hour=14), 3600, User.query.get(2), partner=User.query.get(1))
         task.time_slots.append(ts)
         db.session.add(task)
         db.session.commit()
 
         ###Ready###
-        task = Task('title4', 'detail4', status='READY', price=2, estimate=4, team=Team.query.get(1),start_time=miami.views.get_last_monday().replace(hour=14))
+        task = Task('title4', 'detail4', status='READY', price=2, estimate=4, team=Team.query.get(1), start_time=miami.views.get_last_monday().replace(hour=14))
         ts = TimeSlot(miami.views.get_last_monday().replace(hour=14), 3600, User.query.get(2), partner=User.query.get(1))
         task.time_slots.append(ts)
         db.session.add(task)
         db.session.commit()
 
-        task = Task('title6', 'detail6', status='READY', price=1, estimate=4, team=Team.query.get(1),start_time=miami.views.get_last_monday().replace(hour=10))
+        task = Task('title6', 'detail6', status='READY', price=1, estimate=4, team=Team.query.get(1), start_time=miami.views.get_last_monday().replace(hour=10))
         db.session.add(task)
         db.session.commit()
 
         ###history###
-        task = Task('title5', 'detail5', status='DONE', price=2, estimate=4, team=Team.query.get(1),start_time=(miami.views.get_last_monday() - timedelta(days=5)).replace(hour=14))
+        task = Task('title5', 'detail5', status='DONE', price=2, estimate=4, team=Team.query.get(1), start_time=(miami.views.get_last_monday() - timedelta(days=5)).replace(hour=14))
         ts = TimeSlot((miami.views.get_last_monday() - timedelta(days=5)).replace(hour=14), 3600, User.query.get(2))
         task.time_slots.append(ts)
         db.session.add(task)
@@ -70,8 +70,7 @@ class ReviewModelsTest(unittest.TestCase):
         self.assertEquals(5, rd.working_hours)
         self.assertEquals(4, rd.valuable_hours)
         self.assertEquals(3, rd.paired_time)
-        self.assertEquals("[['$1', 0], ['$2', 3], ['$5', 0], ['$10', 0]]",rd.price_ratio())
-
+        self.assertEquals("[['$1', 0], ['$2', 3], ['$5', 0], ['$10', 0]]", rd.price_ratio())
 
     def test_personal_review_data(self):
         member = User.query.get(2)
@@ -94,17 +93,17 @@ class ReviewModelsTest(unittest.TestCase):
         self.assertEquals(0, rd.estimate)
         self.assertEquals(0, rd.working_hours)
         self.assertEquals(0, rd.valuable_hours)
-        self.assertEquals(0,rd.paired_time)
+        self.assertEquals(0, rd.paired_time)
 
     def test_personal_card(self):
         member = User.query.get(2)
 
         pc = member.personal_card()
 
-        self.assertEquals(6,pc.fortune)
-        self.assertEquals(4,pc.working_hours)
-        self.assertEquals(3,pc.valuable_hours)
-        self.assertEquals(8,pc.estimate)
-        self.assertEquals(3,pc.paired_hours)
-        self.assertEquals('37.50%',pc.estimate_deviation())
+        self.assertEquals(6, pc.fortune)
+        self.assertEquals(4, pc.working_hours)
+        self.assertEquals(3, pc.valuable_hours)
+        self.assertEquals(8, pc.estimate)
+        self.assertEquals(3, pc.paired_hours)
+        self.assertEquals('37.50%', pc.estimate_deviation())
         self.assertEquals('75.00%',pc.paired_ratio())
