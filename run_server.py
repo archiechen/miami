23c4691353530eedf764a72aa6cdbec509b9eb0b
archiechen <chenyachuan@hotmail.com>
2012-11-26 08:15:52 +0800

    修复review固定了Teamid的bug

--- run_server.py
+++ run_server.py
@@ -9,7 +9,7 @@ def main():
     # Start the scheduler
     sched = Scheduler()
     sched.start()
-    sched.add_cron_job(miami.zeroing, hour='15', minute='19')
+    sched.add_cron_job(miami.zeroing, hour='23')
     miami.app.run()
 
 if __name__ == "__main__":
